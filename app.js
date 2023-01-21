43df869589f6a18290042f257877bcbe159f335a
Tonmoysifat <98707041+Tonmoysifat@users.noreply.github.com>
2023-01-22 02:17:19 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 AOS.init();
-var icon=document.getElementById("toggle-btn");
-if ( localStorage.getItem("Theme")==null ) {
-    localStorage.setItem("Theme","light");
+var icon = document.getElementById("toggle-btn");
+if (localStorage.getItem("Theme") == null) {
+    localStorage.setItem("Theme", "light");
 }
-
-let localData=localStorage.getItem("Theme");
+let localData = localStorage.getItem("Theme");
 if (localData == "light") {
-    icon.innerHTML="Dark";
+    icon.innerHTML = "Dark";
     // icon.src="images/moon.png";
     document.body.classList.remove("dark-theme");
+    nevi.style.backgroundColor = "white";
 }
 else if (localData == "dark") {
-    icon.innerHTML="Light";
+    icon.innerHTML = "Light";
     // icon.src="images/sun.png";
     document.body.classList.add("dark-theme");
+    nevi.style.backgroundColor = "";
 }
-icon.onclick = function(){
+icon.onclick = function () {
     document.body.classList.toggle("dark-theme");
     if (document.body.classList.contains("dark-theme")) {
-    // icon.src="images/sun.png";
-    icon.innerHTML="Light";
-    localStorage.setItem("Theme","dark");
-} else {
-    // icon.src="images/moon.png";
-   icon.innerHTML="Dark";
-    localStorage.setItem("Theme","light");
-}
+        // icon.src="images/sun.png";
+        nevi.style.backgroundColor = "";
+        icon.innerHTML = "Light";
+        localStorage.setItem("Theme", "dark");
+    } else {
+        // icon.src="images/moon.png";
+        nevi.style.backgroundColor = "white";
+        icon.innerHTML = "Dark";
+        localStorage.setItem("Theme", "light");
+    }
+
 }
