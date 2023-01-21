b9cc86f41d8fc93b6beaedc8d9f2f205ba6407d5
Tonmoysifat <98707041+Tonmoysifat@users.noreply.github.com>
2023-01-21 22:07:45 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 AOS.init();
+var icon=document.getElementById("toggle-btn");
+if ( localStorage.getItem("Theme")==null ) {
+    localStorage.setItem("Theme","light");
+}
+
+let localData=localStorage.getItem("Theme");
+if (localData == "light") {
+    icon.innerHTML="Dark";
+    // icon.src="images/moon.png";
+    document.body.classList.remove("dark-theme");
+}
+else if (localData == "dark") {
+    icon.innerHTML="Light";
+    // icon.src="images/sun.png";
+    document.body.classList.add("dark-theme");
+}
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+    // icon.src="images/sun.png";
+    icon.innerHTML="Light";
+    localStorage.setItem("Theme","dark");
+} else {
+    // icon.src="images/moon.png";
+   icon.innerHTML="Dark";
+    localStorage.setItem("Theme","light");
+}
+}
